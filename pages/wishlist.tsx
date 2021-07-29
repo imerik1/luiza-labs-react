03c8dc3207585f63b961db986ea342d3aa6a6b11
Erik Santana <erik_s_marques@hotmail.com>
2021-07-29 13:17:41 -0300

    feat: pages wish list

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -3,12 +3,20 @@ import {
   useState,
 } from "react";
 
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
 import { Breadcrumbs } from "../components/Breadcrumbs/BreadcrumbsComponent";
 import { HeaderComponent } from "../components/Header/HeaderComponent";
+import {
+  WishComponentComponent,
+} from "../components/WishList/WishListComponent";
+import { IProducts } from "../types/products";
 
-export default function WishList() {
+type Props = {
+  products: Array<IProducts>;
+};
+export default function WishList({ products }: Props) {
   const [useLocalization, setLocalization] = useState<
     string | null | undefined
   >();
@@ -29,6 +37,15 @@ export default function WishList() {
           { href: "/wishlist", breadcrumb: "Lista de Desejos" },
         ]}
       />
+      <WishComponentComponent products={products} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(
+    "https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e"
+  );
+  const products: Props = await res.json();
+  return { props: products };
+};
