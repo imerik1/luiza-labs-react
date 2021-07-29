62e4654d71e8161ff8c020764dc5e5b927c3479e
Erik Santana <erik_s_marques@hotmail.com>
2021-07-28 22:14:24 -0300

    feat: prepare to create a change locate function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import {
+  useEffect,
+  useState,
+} from 'react';
+
+import Head from 'next/head';
+
 import { HeaderComponent } from '../components/HeaderComponent';
 
 export default function Index() {
+  const [useLocalization, setLocalization] = useState<
+    string | null | undefined
+  >();
+  useEffect(() => {
+    if (localStorage.getItem("localization")) {
+      setLocalization(localStorage.getItem("localization"));
+    } else {
+    }
+  }, []);
   return (
     <>
-      <HeaderComponent title="MegaNets"/>
+      <Head>
+        <title>Página inicial</title>
+      </Head>
+      <div className="choose"></div>
+      <HeaderComponent title="MegaNets" localization={useLocalization} />
     </>
   );
 }
