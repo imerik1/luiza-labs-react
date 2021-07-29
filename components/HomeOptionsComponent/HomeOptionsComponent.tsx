29f836564111d1ec3728edbf25e6173093a6cf73
Erik Santana <erik_s_marques@hotmail.com>
2021-07-29 10:27:32 -0300

    feat: new folder

---
+++ components/HomeOptionsComponent/HomeOptionsComponent.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import { FC } from "react";
+
+import {
+  OptionsListComponent,
+} from "../OptionsListComponent/OptionsListComponent";
+
+type Props = {
+  localization: string | null | undefined;
+};
+
+export const HomeOptionsComponent: FC<Props> = ({ localization }) => {
+  return (
+    <div className="options">
+      <OptionsListComponent
+        link=""
+        src="/assets/map-marker.png"
+        alt="Icone de localização"
+        description={`Cidade: ${localization ? localization : ""}`}
+      />
+      <OptionsListComponent
+        link=""
+        src="/assets/phone.png"
+        alt="Icone de telefone"
+        description="Central de atendimento"
+      />
+      <OptionsListComponent
+        link=""
+        src="/assets/heart.png"
+        alt="Icone de coração"
+        description="Lista de desejos"
+      />
+    </div>
+  );
+};
