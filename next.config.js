da30f0f655e8693f1b41f68deb5641fed580948d
Erik Santana <erik_s_marques@hotmail.com>
2021-07-29 01:02:27 -0300

    fix: fix url external

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     includePaths: [path.join(__dirname, "style")],
   },
   images: {
-    domains: ["https://via.placeholder.com/300x300"],
+    domains: ["via.placeholder.com"],
   },
 };
